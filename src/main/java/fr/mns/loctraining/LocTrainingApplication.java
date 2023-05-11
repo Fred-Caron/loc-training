96b5ac893402333202fd9576bffcf58509a36576
fcaron <frederic.caron@businessdecision.lu>
2023-05-11 17:13:29 +0200

    Spring Initializr

---
+++ src/main/java/fr/mns/loctraining/LocTrainingApplication.java
@@ -0,0 +1,13 @@
+package fr.mns.loctraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocTrainingApplication.class, args);
+	}
+
+}
