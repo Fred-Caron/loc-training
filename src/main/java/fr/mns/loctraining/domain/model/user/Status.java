77926a36cc217086f23fbd1222dc14989e7d2b0f
fcaron <frederic.caron@businessdecision.lu>
2023-05-15 13:49:54 +0200

    Entity explications

--- src/main/java/fr/mns/loctraining/domain/model/user/Status.java
+++ src/main/java/fr/mns/loctraining/domain/model/user/Status.java
@@ -8,8 +8,11 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+// L'annotation sert à indiquer que c'est une entity, c'est à dire une classe Java qui représente une table de ta DB, ici la table status
 @Entity
+// L'annotation Lombok Getter sert à générer les getters lors de la compilation
 @Getter
+// L'annotation Lombok Setter sert à générer les setters lors de la compilation
 @Setter
 public class Status extends BaseEntity {
     private String name;
